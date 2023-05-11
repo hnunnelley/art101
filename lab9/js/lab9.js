19c11e524701d5831fd0e97a0a9ffc62870a5f27
hanna <hnunnell@ucsc.edu>
2023-05-10 19:11:49 -0700

    javascript for the web

---
+++ lab9/js/lab9.js
@@ -0,0 +1,16 @@
+// index.js - Javascript for the Web
+// Author: Hanna Nunnelley & Melody Caudill
+// Date: 11 May 2023
+
+//getting div elemnt
+var outputEl = document.getElementById("output");
+
+//
+var new1El = document.createElement("p");
+new1El.innerHTML = "HELLLOO!!!";
+outputEl.appendChild(new1El);
+
+//
+var new2El = document.createElement("q");
+new2El.innerHTML = "BYEEE!!";
+outputEl.appendChild(new2El);
