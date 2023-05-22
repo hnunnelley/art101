c012fb8478d9560581de51fc39225923c53200f1
hanna <hnunnell@ucsc.edu>
2023-05-22 15:25:34 -0700

    jQuery stuff

---
+++ lab111/js/lab11.js
@@ -0,0 +1,27 @@
+// index.js - Libraries and jQuery
+// Author: Hanna Nunnelley & Melody Caudill
+// Date: 18 May 2023
+
+
+$(".minor-section").append("<button class='btn btn-secondary highlight-button'>Toggle Highlight</button");
+
+
+$(".highlight-button").click(function(){
+  $(this).parent().toggleClass("special");
+})
+
+$("#party-toggle button").click(function(){
+  if ($("body").hasClass("party")){
+    $("body").removeClass("party");
+    $("#party-toggle").html("Business");
+  }
+})
+
+
+  
+
+
+
+
+
+
--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -3,16 +3,22 @@
 // Date: 11 May 2023
 
 //getting div elemnt
-var outputEl = document.getElementById("output");
+//var outputEl = document.getElementById("output");
 
 //
-var new1El = document.createElement("p");
-new1El.innerHTML = "HELLLOO!!!";
-outputEl.appendChild(new1El);
+//var new1El = document.createElement("p");
+//new1El.innerHTML = "HELLLOO!!!";
+//outputEl.appendChild(new1El);
 
 //
-var new2El = document.createElement("q");
-new2El.innerHTML = "BYEEE!!";
-outputEl.appendChild(new2El);
+//var new2El = document.createElement("q");
+//new2El.innerHTML = "BYEEE!!";
+//outputEl.appendChild(new2El);
+
+$(document).ready(function() {
+    var button = $("<button>").text("press me");
+    button.appendTo(".h1");
+  });
+  
     
 
